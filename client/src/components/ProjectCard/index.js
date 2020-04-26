300b573c3f67092f1bae6242b3c3ba7abf16d683
Chadtao206 <chenchen408@gmail.com>
2020-04-26 12:06:03 -0700

    working on project cards

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -1,14 +1,65 @@
 import React from "react";
+import Placehold from "../../assets/project_img_placeholder.jpg";
 
-export default ({ project }) => (
-  <div className="card text-white bg-dark mb-3 col-lg-3 col-md-4 col-sm-6">
-    <div className="card-header">Header</div>
-    <div className="card-body">
-      <h5 className="card-title">Dark card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
+export default ({ project, owner }) => (
+  <div className="flip-card col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div className="flip-card-inner" style={{ height: "40vh" }}>
+      {/* front of card */}
+      <div
+        className="card text-white bg-dark mb-3 mt-2 ml-2 mr-2 flip-card-front"
+        style={{ borderRadius: "10px", height: "100%" }}
+      >
+        <div className="card-header">
+          {owner ? (
+            <span style={{ marginRight: "10%" }}>Your Project!</span>
+          ) : (
+            ""
+          )}
+          {project.contributors.map(a => (
+            <a href={a.url} target="_blank">
+              <span
+                className="badge badge-light mr-1"
+                style={{ color: "dark" }}
+              >
+                {a.handle}
+              </span>
+            </a>
+          ))}
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">{project.name}</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      {/* back of card */}
+      <div
+        className="card text-white bg-dark mb-3 mt-2 ml-2 mr-2 flip-card-back"
+        style={{ borderRadius: "10px", height: "100%" }}
+      >
+        <div className="card-header">
+          {owner ? (
+            <span style={{ marginRight: "10%" }}>Your Project!</span>
+          ) : (
+            ""
+          )}
+          {project.contributors.map(a => (
+            <a href={a.url} target="_blank">
+              <span
+                className="badge badge-light mr-1"
+                style={{ color: "dark" }}
+              >
+                {a.handle}
+              </span>
+            </a>
+          ))}
+        </div>
+        <div className="card-body">
+          <img className="img-fluid" src={Placehold} />
+        </div>
+      </div>
     </div>
   </div>
 );
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -2,13 +2,16 @@ import React from "react";
 import Card from "../components/ProjectCard";
 import projects from "../utils/projects.json";
 
-export default () => {
+export default ({ user }) => {
   return (
     <>
       <h1>Project One</h1>
       <div className="row">
         {projects.project1.map(a => (
-          <Card project={a} />
+          <Card
+            project={a}
+            owner={a.contributors.map(a => a.handle).includes(user.username)}
+          />
         ))}
       </div>
     </>
